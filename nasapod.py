d97b38384eae7588ed1b714c9e37f143b4d04233
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-02-16 13:27:32 -0300

    Update nasapod.py

--- nasapod.py
+++ nasapod.py
@@ -5,25 +5,45 @@ import os
 import sys
 from nasa import apod
 import urllib.request
-from credentials import *
+import requests
+
+# Autentication
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-os.environ['NASA_API_KEY'] = nasa_key
+os.environ['NASA_API_KEY'] = ''
+api_key = ''
+
+# Get the picture/explanation
 
 picture = apod.apod()
 site = str(picture.url)
 explanation = str(picture.explanation)
 
-urllib.request.urlretrieve(site, 'apodtoday.jpeg')
-
-
-toReply = "user" #user to get most recent tweet
+toReply = "" # user to get most recent tweet
 
 site = picture.url
 title = picture.title
+
+# Get the video thumbnail
+
+URL_APOD = "https://api.nasa.gov/planetary/apod"
+params = {
+      'api_key':api_key,
+      'hd':'True',
+      'thumbs':'True'
+  }
+response = requests.get(URL_APOD,params=params).json()
+thumbs = (response.get('thumbnail_url'))
+type = (response.get('media_type'))
+
 
 mystring = f""" Astronomy Picture of the Day
 
@@ -31,15 +51,22 @@ mystring = f""" Astronomy Picture of the Day
 
 Fonte: {site}"""
 
-image = "apodtoday.jpeg"
+# Decide whether is an image or a video and post
 
-try:
+if type == 'image':
+    urllib.request.urlretrieve(site, 'apodtoday.jpeg')
+    image = "apodtoday.jpeg"
     api.update_with_media(image, mystring)
-except:
+elif type == 'video':
+    urllib.request.urlretrieve(thumbs, 'apodvideo.jpeg')
+    video = 'apodvideo.jpeg'
+    api.update_with_media(video, mystring)
+else:
     api.update_status(mystring)
 
 myexstring = f"""{explanation}"""
 
+# Cut the explanation into multiple tweets
 
 def get_chunks(s, maxlength):
     start = 0
@@ -52,11 +79,12 @@ def get_chunks(s, maxlength):
 
 chunks = get_chunks(myexstring, 280)
 
-#Make list with line lengths:
+# Make list with line lengths:
 chunkex = [(n) for n in chunks]
 
 coun = 0
 
+# Post the explanation
 
 while coun < len(chunkex):
     tweets = api.user_timeline(screen_name = toReply, count=1)
