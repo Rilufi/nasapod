5e5319cb6cc868cc35ae70e353ec03135d172d4f
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-19 15:58:43 -0300

    Create rtastro.py

---
+++ rtastro.py
@@ -0,0 +1,30 @@
+import tweepy
+import os
+
+
+#calling secret variables
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+
+#connect on twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+#search hashtag, RT, like and follow
+#three filters: one for only RT the original tweet, other for just media content, safe images and no elon musk
+queries = ['#NASA' ,'#space' ,'#science' ,'#astronomy', '#hubble']
+
+def rtquery(hash):
+    for tweet in tweepy.Cursor(api.search, q=f"{hash} -@elonmusk -elon -musk -spacex -starlink -tesla -filter:retweets -filter:replies filter:images filter:safe").items(1):
+        try:
+            api.create_friendship(tweet.user.screen_name)
+            api.create_favorite(tweet.id)
+            tweet.retweet()
+        except:
+            pass
+    
+for query in queries:
+    rtquery(query)
