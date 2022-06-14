60c885211a684100c98bb3ca6faac681d55c253e
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-06-14 14:22:11 -0300

    Update rtastro.py

--- rtastro.py
+++ rtastro.py
@@ -15,7 +15,7 @@ api = tweepy.API(auth)
 
 #search hashtag, RT, like and follow
 #three filters: one for only RT the original tweet, other for just media content, safe images and no elon musk
-queries = ['#NASA' , '#cosmology' ,'#astronomy', '#hubble', '#astrophotography']
+queries = ['#NASA' , '#cosmology' ,'#astronomy', '#hubble', '#astrophotography', '#AstroMiniBR', '#AstroThreadBR']
 
 def rtquery(hash):
     for tweet in tweepy.Cursor(api.search, q=f"{hash} -alien -uaptwitter -uap -ufotwitter -quran -metaverse -hubblers -workspace -coverup -flat -flatearther -flatearth -astrology -astrologer -$hbb -OVNI -UFO -@elonmusk -elonmusk -elon -musk -spacex -starlink -tesla -filter:retweets -filter:replies filter:images filter:safe",  result_type="recent").items(1):
