952153618eb4c1eff3804df918d1057cf98615ab
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 08:34:18 -0300

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,14 @@
+import tweepy
+import os
+
+
+#calling secret variables
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+
+#connect on twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
