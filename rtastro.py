6876a0e0dca622d6de9fc641ff0bee31704a3054
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 08:33:53 -0300

    Update rtastro.py

--- rtastro.py
+++ rtastro.py
@@ -1,17 +1,5 @@
 import tweepy
-import os
-
-
-#calling secret variables
-CONSUMER_KEY = os.environ["CONSUMER_KEY"]
-CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
-
-#connect on twitter
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
+from auth import api
 
 #search hashtag, RT, like and follow
 #three filters: one for only RT the original tweet, other for just media content, safe images and no elon musk
