7fe489346087e655db3b7709b104744b018bbe1d
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-08-11 16:04:10 -0300

    Update twitter_unfollow.py

--- twitter_unfollow.py
+++ twitter_unfollow.py
@@ -3,7 +3,7 @@ from auth import api
 
 class unfollow:
     def unfollow(self):
-        my_screen_name = api.verify_credentials().screen_name
+        my_screen_name = api.me().screen_name
         user = api.get_user(screen_name=my_screen_name)
         print('\n--------------------')
         print(user.name)
@@ -11,7 +11,7 @@ class unfollow:
         print(my_screen_name + ' has ' + str(user.friends_count) + ' friends')
         if user.friends_count > 3000:
             inactive_friends = [];
-            friends = api.get_friend_ids(screen_name=my_screen_name)
+            friends = api.friends_ids(screen_name=my_screen_name)
             for friend in friends[::-1]:
                 tweets_list= api.user_timeline(user_id = friend, exclude_replies = 'true', include_rts = 'false', count = 1)
                 try:
@@ -28,7 +28,7 @@ class unfollow:
             if (len(inactive_friends) > 0):
                 print('Unfollowing %s friends..' % len(inactive_friends))
                 for friend in inactive_friends:
-                    api.destroy_friendship(user_id=friend)
+                    api.destroy_friendship(friend)
         elif user.friends_count <= 3000:
             print(f"sem unfollow p/ {my_screen_name} hoje")
         else:
