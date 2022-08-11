aa9153812165fb9dd869989572f367ebdb3fa32f
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-08-11 15:56:41 -0300

    Update twitter_unfollow.py

--- twitter_unfollow.py
+++ twitter_unfollow.py
@@ -1,43 +1,35 @@
-# Randomly unfollows users if the authenticating user's following number is greater than 3000 until it's 2900
-
+import tweepy
 from auth import api
-import random, time, tweepy
 
 class unfollow:
-    my_screen_name = api.me().screen_name
-
     def unfollow(self):
-
-        if api.me().friends_count > 3000:
-            print(f'Current following count is at {api.me().friends_count}.')
-            for count, friend in enumerate(tweepy.Cursor(api.friends).items(api.me().friends_count)):
-                
-                if count <= 500:
-                    print(f'{count}. User was recently followed.' )
-                    continue
-                
-                # Checks if user is following authenticating user
-                elif self.following_me(friend.screen_name):
-                    print(f'{count}. {friend.screen_name} follows {self.my_screen_name}.')
-                    continue
-                
-                # Randomly skips users
-                elif random.randint(0, 1) == 1:
-                    print(f'{count}. User has been skipped.')
-                    continue
-                
-                # Ends loop if following count is 1500
-                elif api.me().friends_count == 2900:
-                    break
-                
-                else:
-                    api.destroy_friendship(friend.screen_name)
-                    print(f'{count}. {friend.screen_name} has been unfollowed.')
-                    time.sleep(2.5)
+        my_screen_name = api.verify_credentials().screen_name
+        user = api.get_user(screen_name=my_screen_name)
+        print('\n--------------------')
+        print(user.name)
+        print('--------------------')
+        print(my_screen_name + ' has ' + str(user.friends_count) + ' friends')
+        if user.friends_count > 3000:
+            inactive_friends = [];
+            friends = api.get_friend_ids(screen_name=my_screen_name)
+            for friend in friends[::-1]:
+                tweets_list= api.user_timeline(user_id = friend, exclude_replies = 'true', include_rts = 'false', count = 1)
+                try:
+                    tweet = tweets_list[0]
+                    delta = date.today() - tweet.created_at.date()
+                    if (len(inactive_friends) >= user.friends_count - 3000):
+                        break
+                    elif (delta.days < 30):
+                        continue
+                    else:
+                        inactive_friends.append(friend)
+                except:
+                    pass
+            if (len(inactive_friends) > 0):
+                print('Unfollowing %s friends..' % len(inactive_friends))
+                for friend in inactive_friends:
+                    api.destroy_friendship(user_id=friend)
+        elif user.friends_count <= 3000:
+            print(f"sem unfollow p/ {my_screen_name} hoje")
         else:
-            print(f'Following count is at {api.me().friends_count}. No unfollowing needed.')
-    
-    # Checks if a user is following the authenticating user
-    def following_me(self, screen_name):
-        status = api.show_friendship(source_screen_name = screen_name, target_screen_name = self.my_screen_name)
-        return status[0].following
+            print(f"tentei p/ {my_screen_name}, mas não rolou.")
