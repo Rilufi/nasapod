cac6432f75b7022eb60c4e046897b5ebb22e8cc9
Yuri <xamexavu@hotmail.com>
2023-06-14 16:21:29 -0300

    Update rtastro.py

--- rtastro.py
+++ rtastro.py
@@ -8,16 +8,17 @@ import random
 queries = ['#NASA' , '#cosmology' ,'#astronomy', '#hubble', '#astrophotography', '#AstroMiniBR', '#AstroThreadBR']
 
 def rtquery(hash):
-    for tweet in tweepy.Cursor(api.search, q=f"{hash} -God -Jesus -metta -RoboticDogs -RBIF -positivethinking -goodvibes -healing -belief -lifeforms -alien -uaptwitter -uap -ufotwitter -quran -metaverse -hubblers -workspace -coverup -flat -flatearther -flatearth -astrology -astrologer -$hbb -OVNI -UFO -@elonmusk -elonmusk -elon -musk -spacex -starlink -tesla -filter:retweets -filter:replies filter:images filter:safe",  result_type="recent").items(1):
-        try:
-            api.create_friendship(tweet.user.screen_name)
-            api.create_favorite(tweet.id)
-            tweet.retweet()
-            print(f"{hash} rolou.")
-        except:
-            print(f"{hash} já foi.")
-            pass
-    
+#    for tweet in tweepy.Cursor(api.search, q=f"{hash} -God -Jesus -metta -RoboticDogs -RBIF -positivethinking -goodvibes -healing -belief -lifeforms -alien -uaptwitter -uap -ufotwitter -quran -metaverse -hubblers -workspace -coverup -flat -flatearther -flatearth -astrology -astrologer -$hbb -OVNI -UFO -@elonmusk -elonmusk -elon -musk -spacex -starlink -tesla -filter:retweets -filter:replies filter:images filter:safe",  result_type="recent").items(1):
+    tweet = api.search_recent_tweets(f"{hash} -God -Jesus -metta -RoboticDogs -RBIF -positivethinking -goodvibes -healing -belief -lifeforms -alien -uaptwitter -uap -ufotwitter -quran -metaverse -hubblers -workspace -coverup -flat -flatearther -flatearth -astrology -astrologer -$hbb -OVNI -UFO -@elonmusk -elonmusk -elon -musk -spacex -starlink -tesla -filter:retweets -filter:replies filter:images filter:safe", max_results=1)#.items(1):
+    try:
+        api.create_friendship(tweet.user.screen_name)
+        api.like(tweet.id)
+        tweet.retweet()
+        print(f"{hash} rolou.")
+    except:
+        print(f"{hash} já foi.")
+        pass
+
 for query in queries:
     rtquery(query)
 
