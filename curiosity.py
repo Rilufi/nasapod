5f559ec7f65adc54826cf59ec99e2a2fa34ad40d
Yuri <xamexavu@hotmail.com>
2023-06-20 11:20:26 -0300

    Update curiosity.py

--- curiosity.py
+++ curiosity.py
@@ -1,14 +1,34 @@
 import tweepy
 import os
+import sys
+import urllib.request
+import requests
 from auth import api, client
-from marstuff import Client
 
-# import keys
-api_key = os.environ.get("API_KEY")
-client = Client(api_key) 
+# Autentication
+api_key = os.environ["API_KEY"]
 
-# Get the latest Photo
-photo = client.curiosity.get_latest_photo()
-media = api.media_upload(photo)
+# Get the picture, explanation and/or video thumbnail
+URL_APOD = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos"
+params = {
+      'api_key':api_key,
+  }
+response = requests.get(URL_APOD, params=params).json()
+latest_photos = response.get('latest_photos')
+latest_photo = latest_photos[0]
 
-client.create_tweet(text="Latest photo taken by the Curiosity Rover", media_ids=[media.media_id])
+camera = latest_photo['camera']['full_name']
+sol = latest_photo['sol']
+site = latest_photo['img_src']
+
+mystring = f""" Latest Curiosity Rover Picture
+
+Taken from the {camera} on Sol {sol}
+
+Source: {site}"""
+
+print(mystring)
+urllib.request.urlretrieve(site, 'rovertoday.jpeg')
+image = "rovertoday.jpeg"
+media = api.media_upload(image)
+client.create_tweet(text=mystring, media_ids=[media.media_id])
