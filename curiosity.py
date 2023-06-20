6ac1cbbfd4d98c5bacf278bb812e22fa517c210a
Yuri <xamexavu@hotmail.com>
2023-06-20 12:52:08 -0300

    Update curiosity.py

--- curiosity.py
+++ curiosity.py
@@ -8,42 +8,42 @@ import datetime
 
 
 today = datetime.date.today()
+hoje = today.strftime('%Y-%m-%d')
 api_key = os.environ["API_KEY"]
+#api_key = "DEMO_KEY"
 
-def rover_pic(URL_APOD, rover):
+def rover_pic(URL_APOD, rover_name):
     # Get the picture after specifying the rover
     params = {
           'api_key':api_key,
-          'max_date':today
       }
     response = requests.get(URL_APOD, params=params).json()
-    latest_photos = response.get('rover')
-    latest_photo = latest_photos[0]
-    camera = latest_photo['camera']['full_name']
-    sol = latest_photo['sol']
-    site = latest_photo['img_src']
+    rover = response.get('rover')
+    max_date = rover['max_date']
+    if max_date == hoje:
+        URL_APOD = URL_APOD+'latest_photos'
+        response = requests.get(URL_APOD, params=params).json()
+        latest_photos = response.get('latest_photos')
+        latest_photo = latest_photos[0]
+        camera = latest_photo['camera']['full_name']
+        sol = latest_photo['sol']
+        site = latest_photo['img_src']
 
-    mystring = f""" Latest {rover} Rover Picture
+        mystring = f""" Latest {rover_name} Rover Picture
 
 Taken from the {camera} on Sol {sol}
 
 Source: {site}"""
 
-    urllib.request.urlretrieve(site, 'rovertoday.jpeg')
-    image = "rovertoday.jpeg"
-    media = api.media_upload(image)
-    client.create_tweet(text=mystring, media_ids=[media.media_id])
-#    print(mystring)
+        urllib.request.urlretrieve(site, 'rovertoday.jpeg')
+        image = "rovertoday.jpeg"
+        media = api.media_upload(image)
+        client.create_tweet(text=mystring, media_ids=[media.media_id])
+        print(mystring)
+    else:
+        print(f'Sem {rover_name} hoje')
+        pass
 
-try:
-    rover_pic("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity", 'Curiosity')
-except:
-    print('Curiosity já foi.')
-try:
-    rover_pic("https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance", 'Perseverance')
-except:
-    print('Perseverance já foi.')
-try:
-    rover_pic("https://api.nasa.gov/mars-photos/api/v1/rovers/spirit", 'Spirit')
-except:
-    print('Spirit já foi.')
+rover_pic("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/", 'Curiosity')
+rover_pic("https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/", 'Perseverance')
+rover_pic("https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/", 'Spirit')
