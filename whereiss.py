2fdf8157b352cf91f1a355a943d199c5d9640214
Yuri <xamexavu@hotmail.com>
2023-06-21 20:00:57 -0300

    Update whereiss.py

--- whereiss.py
+++ whereiss.py
@@ -21,16 +21,3 @@ if location is not None and location.raw.get("address") is not None:
     print("País: ", country)
 else:
     print("Não foi possível determinar a localização.")
-
-# Creating the world map
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-
-# Plotting the world map
-fig, ax = plt.subplots(figsize=(10, 6))
-world.plot(ax=ax)#, color='lightgray')
-
-# Plotting the ISS location
-ax.plot(longitude, latitude, 'ro', markersize=6)
-
-# Saving the map as PNG
-plt.savefig('iss_map.png', dpi=300, bbox_inches='tight')
