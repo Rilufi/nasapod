ae246847ed726aed7bc9b3e6c110678d64243c5a
Yuri <xamexavu@hotmail.com>
2023-06-21 20:26:47 -0300

    Update whereiss.py

--- whereiss.py
+++ whereiss.py
@@ -4,6 +4,7 @@ import urllib.request
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from geopy.geocoders import Nominatim
+from auth import api, client
 
 # Fetching ISS info
 url = "http://api.open-notify.org/iss-now.json"
@@ -21,13 +22,6 @@ geolocator.headers = {'Authorization': Nominatin_Key}
 coordinates = (latitude, longitude)
 location = geolocator.reverse(coordinates, exactly_one=True)
 
-if location is not None and location.raw.get("address") is not None:
-    address = location.raw["address"]
-    country = address.get("country")
-    print("País: ", country)
-else:
-    print("Não foi possível determinar a localização.")
-
 # Creating the world map
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
@@ -40,3 +34,20 @@ ax.plot(longitude, latitude, 'ro', markersize=6)
 
 # Saving the map as PNG
 plt.savefig('iss_map.png', dpi=300, bbox_inches='tight')
+image = "iss_map.png"
+media = api.media_upload(image)
+
+if location is not None and location.raw.get("address") is not None:
+    address = location.raw["address"]
+    country = address.get("country")
+    print("País: ", country)
+    mystring = f""" ISS Tracker
+
+The International Space Station currently position is above {country}"""
+    client.create_tweet(text=mystring, media_ids=[media.media_id])
+else:
+    mystring = """ ISS Tracker
+
+The International Space Station currently position:"""
+    print("Não foi possível determinar a localização.")
+    client.create_tweet(text=mystring, media_ids=[media.media_id])
