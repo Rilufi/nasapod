68e8e3b2cf1392f9df69844eda504b9256a098fc
Yuri <xamexavu@hotmail.com>
2024-03-28 09:40:11 -0300

    Update storier.py

--- storier.py
+++ storier.py
@@ -1,34 +1,20 @@
 import os
-from instagrapi import Client
-from instagrapi.exceptions import ClientError
-import time
+from instapy import InstaPy
+
 
 # Authentication
 username = os.environ.get("USERNAME")
 password = os.environ.get("PASSWORD")
 
-# Initialize the client
-client = Client()
-client.login(username, password)  # Log in to your account
+# Crie uma instância do InstaPy
+session = InstaPy(username=username, password=password)
 
-# ID of NASA's official Instagram account
-nasa_user_id = "528817151"  # ID of NASA's official account
+# Inicie a sessão
+session.login()
 
-# Get followers of NASA's official account
-try:
-    followers = client.user_followers(nasa_user_id, amount=50)  # Get 50 followers of NASA
-    for follower in followers:
-        # Check if the follower object is a string (username) or a dictionary (follower)
-        if isinstance(follower, str):
-            user_info = client.user_info_by_username(follower)
-            client.user_follow(user_info.pk)
-            print(f"Following {user_info.username}")
-        else:
-            client.user_follow(follower["pk"])
-            print(f"Following {follower['username']}")
-        time.sleep(360)  # Wait 6 minutes before following the next follower (50 followers in 5 hours)
-except ClientError as e:
-    print(f"Error getting NASA's followers: {e}")
+# Defina as ações
+session.like_by_tags(['astronomy', 'space'], amount=100)
+session.set_do_follow(True, percentage=50)
 
-# Logout
-client.logout()
+# Finalize a sessão
+session.end()
