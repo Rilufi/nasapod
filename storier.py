f3d3e685ff37271ab77fa2a46538bde3d16510d8
Yuri <xamexavu@hotmail.com>
2024-03-27 22:56:45 -0300

    Update storier.py

--- storier.py
+++ storier.py
@@ -1,29 +1,34 @@
+import os
 from instagrapi import Client
 from instagrapi.exceptions import ClientError
 import time
-import os
 
 # Authentication
 username = os.environ.get("USERNAME")
 password = os.environ.get("PASSWORD")
 
-# Inicialize o cliente
+# Initialize the client
 client = Client()
-client.login(username, password)  # Faça login na sua conta
+client.login(username, password)  # Log in to your account
 
-# ID da conta oficial da NASA no Instagram
-nasa_user_id = "1518284433"  # ID da conta oficial da NASA
+# ID of NASA's official Instagram account
+nasa_user_id = "1518284433"  # ID of NASA's official account
 
-# Obtenha os seguidores da conta oficial da NASA
+# Get followers of NASA's official account
 try:
-    followers = client.user_followers(nasa_user_id, amount=50)  # Obtenha 50 seguidores da NASA
+    followers = client.user_followers(nasa_user_id, amount=50)  # Get 50 followers of NASA
     for follower in followers:
-        # Siga o seguidor
-        client.user_follow(follower.pk)
-        print(f"Seguindo {follower.username}")
-        time.sleep(360)  # Espere 6 minutos antes de seguir o próximo seguidor (50 seguidores em 5 horas)
+        # Check if the follower object is a string (username) or a dictionary (follower)
+        if isinstance(follower, str):
+            user_info = client.user_info_by_username(follower)
+            client.user_follow(user_info.pk)
+            print(f"Following {user_info.username}")
+        else:
+            client.user_follow(follower["pk"])
+            print(f"Following {follower['username']}")
+        time.sleep(360)  # Wait 6 minutes before following the next follower (50 followers in 5 hours)
 except ClientError as e:
-    print(f"Erro ao obter seguidores da NASA: {e}")
+    print(f"Error getting NASA's followers: {e}")
 
 # Logout
 client.logout()
