4e69df5497d8687c3c01c135de3e6f6f36fcaac3
Yuri <xamexavu@hotmail.com>
2024-05-30 20:14:53 -0300

    Update threader.py

--- threader.py
+++ threader.py
@@ -1,8 +1,138 @@
+# coding=utf-8
 import os
+import sys
+import urllib.request
+import requests
+import time
+import google.generativeai as genai
+from pytube import YouTube
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from datetime import datetime, timedelta
 from threadspy import ThreadsAPI
 
 
+# Authentication
+api_key = os.environ.get("API_KEY")
 username = os.environ.get("USERNAME")
 password = os.environ.get("PASSWORD")
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+genai.configure(api_key=GOOGLE_API_KEY)
 api = ThreadsAPI(username=username, password=password)
-api.publish(caption="Primeiro teste funcionou, yay. Testando agora a automação (se esse for, o próximo teste já vai ser os rolê da nasa)")
+
+
+# Choose a GenAI model (e.g., 'gemini-pro')
+model = genai.GenerativeModel('gemini-pro')
+
+# Get the picture, explanation, and/or video thumbnail
+URL_APOD = "https://api.nasa.gov/planetary/apod"
+params = {
+    'api_key': api_key,
+    'hd': 'True',
+    'thumbs': 'True'
+}
+response = requests.get(URL_APOD, params=params).json()
+site = response.get('url')
+thumbs = response.get('thumbnail_url')
+type = response.get('media_type')
+explanation = response.get('explanation')
+title = response.get('title')
+hashtags = "#NASA #APOD #Astronomy #Space #Astrophotography"
+
+
+# Função para gerar conteúdo traduzido usando o modelo GenAI
+def gerar_traducao(prompt):
+    response = model.generate_content(prompt)
+    if response.candidates and len(response.candidates) > 0:
+        if response.candidates[0].content.parts and len(response.candidates[0].content.parts) > 0:
+            return response.candidates[0].content.parts[0].text
+        else:
+            print("Nenhuma parte de conteúdo encontrada na resposta.")
+    else:
+        print("Nenhum candidato válido encontrado.")
+    return None
+
+
+# Combinar o título e a explicação em um único prompt
+prompt_combinado = f"Given the following scientific text from a reputable source (NASA) in English, translate it accurately and fluently into grammatically correct Brazilian Portuguese while preserving the scientific meaning:\n{title}\n{explanation}"
+
+try:
+    traducao_combinada = gerar_traducao(prompt_combinado)
+    titulo_traduzido, explicacao_traduzida = traducao_combinada.split('\n', 1)
+
+    insta_string = f"""Foto Astronômica do Dia
+{titulo_traduzido}
+
+{explicacao_traduzida}
+
+Fonte: {site}
+
+#NASA #APOD #Astronomia #Espaço #Astrofotografia"""
+
+except AttributeError:
+    insta_string = f"""Astronomy Picture of the Day
+{title}
+
+{explanation}
+
+Source: {site}
+
+{hashtags}"""
+
+print(insta_string)
+
+mystring = f"""Astronomy Picture of the Day
+
+{title}
+
+Source: {site}
+
+{hashtags}"""
+
+myexstring = f"""{explanation}"""
+
+
+# Função para baixar o vídeo e retornar o nome do arquivo baixado
+def download_video(link):
+    try:
+        youtube_object = YouTube(link)
+        video_stream = youtube_object.streams.get_highest_resolution()
+        if video_stream:
+            video_filename = video_stream.default_filename
+            video_stream.download()
+            return video_filename  # Retorna o nome do arquivo do vídeo baixado
+        else:
+            print("Nenhuma stream encontrada para o vídeo.")
+            return None
+    except Exception as e:
+        print(f"Erro ao baixar o vídeo: {e}")
+        return None  # Retorna None se o download falhar
+
+# Função para cortar o vídeo
+def cortar_video(video_path, start_time, end_time, output_path):
+    try:
+        with VideoFileClip(video_path) as video:
+            video_cortado = video.subclip(start_time, end_time)
+            video_cortado.write_videofile(output_path, codec="libx264")
+        return output_path
+    except Exception as e:
+        print(f"Erro ao cortar o vídeo: {e}")
+        return None
+
+
+if type == 'image':
+
+  # Post the image on Twitter
+    urllib.request.urlretrieve(site, 'apodtoday.jpeg')
+    image = "apodtoday.jpeg"
+    # Post the image on Threads
+    try:
+        api.publish(caption=mystring, image_path=image)
+    except Exception as e:
+        print(f"Error posting photo on Instagram: {e}")
+
+
+elif type == 'video':
+    pass
+
+else:
+    print("Tipo de mídia inválido.")
