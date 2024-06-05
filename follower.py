86a639a41e6a8e03c116dc2972a0d03d705b9183
Yuri <xamexavu@hotmail.com>
2024-06-05 18:17:12 -0300

    Update follower.py

--- follower.py
+++ follower.py
@@ -10,11 +10,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
 options = [
+#    "--headless",
     "--disable-gpu",
     "--window-size=1920,1200",
     "--ignore-certificate-errors",
@@ -79,6 +81,7 @@ def find_page(driver):
         print(f"Página {page_name} não está disponível")
         return False
 
+
 def follow_all(driver):
     try:
         print("Procurando botão de seguidores")
@@ -94,29 +97,55 @@ def follow_all(driver):
 
         # Esperar a lista de seguidores carregar completamente
         print("Esperando a lista de seguidores carregar")
-        # Esperar até que ao menos um seguidor esteja visível na lista
         WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'x1dm5mii')]"))
         )
 
-        print("Procurando elementos de seguidores")
-        followers = driver.find_elements(By.XPATH, "//div[contains(@class, 'x1dm5mii')]")
+        count = 0
+        while count < 8:
+            # Procurar elementos de seguidores
+            print("Procurando elementos de seguidores")
+            followers = driver.find_elements(By.XPATH, "//div[contains(@class, 'x1dm5mii')]")
 
-        if not followers:
-            raise TimeoutException("Não foi possível encontrar elementos de seguidores")
+            if not followers:
+                raise TimeoutException("Não foi possível encontrar elementos de seguidores")
 
-        print("Elementos de seguidores encontrados")
+            print("Elementos de seguidores encontrados")
 
-        # Seguir apenas os primeiros 8 seguidores
-        for follower in followers[:8]:
-            try:
-                follow_button = follower.find_element(By.TAG_NAME, value='button')
-                follow_button.click()
-                print("Seguindo um seguidor")
-                sleep(2)  # Aguardar 2 segundos entre os cliques para evitar ser bloqueado pelo Instagram
-            except NoSuchElementException:
-                print("Botão de seguir não encontrado para um dos seguidores")
-                continue
+            # Seguir os seguidores que ainda não estão sendo seguidos
+            for follower in followers:
+                if count >= 8:
+                    break
+                try:
+                    follow_button = follower.find_element(By.TAG_NAME, value='button')
+                    if follow_button.text == 'Follow':
+                        follow_button.click()
+                        print("Seguindo um seguidor")
+                        sleep(2)  # Aguardar 2 segundos entre os cliques para evitar ser bloqueado pelo Instagram
+                        count += 1
+                    else:
+                        print("Este seguidor já está sendo seguido")
+                except NoSuchElementException:
+                    print("Botão de seguir não encontrado para um dos seguidores")
+                    continue
+
+            if count < 8:
+                # Enviar teclas TAB e PAGE_DOWN para descer a lista de seguidores
+                print("Pressionando Tab e depois Page Down para descer na lista de seguidores")
+                body = driver.find_element(By.TAG_NAME, 'body')
+                for _ in range(3):
+                    body.send_keys(Keys.TAB)
+                    sleep(1)
+                body.send_keys(Keys.PAGE_DOWN)
+                sleep(2)
+
+                # Verificar se novos seguidores foram carregados
+                new_followers = driver.find_elements(By.XPATH, "//div[contains(@class, 'x1dm5mii')]")
+                new_followers_count = len(new_followers)
+                print(f"Foram carregados {new_followers_count - len(followers)} novos seguidores")
+                if new_followers_count == len(followers):
+                    print("Não há mais novos seguidores para carregar")
+                    break
 
         print('done')
 
