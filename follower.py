d0a7596614315a0e4c81b2789192c61181bef93e
Yuri <xamexavu@hotmail.com>
2024-06-05 14:41:03 -0300

    Update follower.py

--- follower.py
+++ follower.py
@@ -2,7 +2,7 @@ from credentials import credential_dict
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
@@ -10,8 +10,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException, NoSuchElementException, UnexpectedAlertPresentException, NoAlertPresentException
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
@@ -30,40 +28,54 @@ for option in options:
 
 def main():
     driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
     driver.get('https://www.instagram.com/')
-    username_field = driver.find_element(By.NAME, value='username').send_keys(credential_dict.get('username'))
-    password_field = driver.find_element(By.NAME, value='password').send_keys(credential_dict.get('password'), Keys.ENTER)
+    
+    try:
+        # Esperar até que o campo de nome de usuário esteja presente
+        username_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'username'))
+        )
+        username_field.send_keys(credential_dict.get('username'))
+        
+        # Esperar até que o campo de senha esteja presente
+        password_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, 'password'))
+        )
+        password_field.send_keys(credential_dict.get('password'), Keys.ENTER)
+        
+    except TimeoutException:
+        print("Não foi possível encontrar os elementos de entrada.")
+        driver.quit()
+    
     driver.implicitly_wait(10)
     sleep(5)
     return driver
 
 def find_page(driver):
     page_name = 'nasabrasil'
-
     url = f'https://www.instagram.com/{page_name}'
     driver.get(url)
     driver.implicitly_wait(10)
-    #check to see if inputted page is real
+    
+    # Verificar se a página não está disponível
     try:
-        driver.find_element(By.XPATH, value='//span[text()="Sorry, this page isn\'t available."]')
+        driver.find_element(By.XPATH, '//span[text()="Sorry, this page isn\'t available."]')
     except NoSuchElementException:
         return True
     else:
         return False
 
 def follow_all(driver):
-    followers_button = driver.find_element(By.XPATH, value="//a[contains(@href, 'followers')]").click()
-    followers = driver.find_elements(By.XPATH, value="//div[contains(@class, 'x1dm5mii')]")
+    followers_button = driver.find_element(By.XPATH, "//a[contains(@href, 'followers')]").click()
+    followers = driver.find_elements(By.XPATH, "//div[contains(@class, 'x1dm5mii')]")
     for follower in followers:
-        button = follower.find_element(By.TAG_NAME, value='button').click()
+        button = follower.find_element(By.TAG_NAME, 'button').click()
     print('done')
 
-
-if  __name__ == "__main__":
+if __name__ == "__main__":
     browser = main()
     valid_page = find_page(browser)
-    if  valid_page:
+    if valid_page:
         follow_all(browser)
     else:
         print('enter valid page')
