0d992891dbaae12aac893433624b76f5619b064d
Yuri <xamexavu@hotmail.com>
2024-06-12 12:48:46 -0300

    Update instasa.py

--- instasa.py
+++ instasa.py
@@ -5,10 +5,7 @@ import requests
 import google.generativeai as genai
 from instagrapi import Client
 import telebot
-from pytube import YouTube
-from moviepy.video.io.VideoFileClip import VideoFileClip
 from datetime import datetime, timedelta
-import openai
 
 # Authentication
 api_key = os.environ.get("API_KEY")
@@ -18,9 +15,7 @@ tele_user = os.environ.get("TELE_USER")
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 bot = telebot.TeleBot(TOKEN)
 GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 genai.configure(api_key=GOOGLE_API_KEY)
-openai.api_key = OPENAI_API_KEY
 
 # Choose a GenAI model (e.g., 'gemini-pro')
 model = genai.GenerativeModel('gemini-pro')
@@ -73,32 +68,17 @@ def post_instagram_photo(cl, image_path, caption):
 
 # Função para gerar conteúdo traduzido usando o modelo GenAI
 def gerar_traducao(prompt):
-    try:
-        response = model.generate_content(prompt)
-        if response.candidates and len(response.candidates) > 0:
-            if response.candidates[0].content.parts and len(response.candidates[0].content.parts) > 0:
-                return response.candidates[0].content.parts[0].text
-            else:
-                print("Nenhuma parte de conteúdo encontrada na resposta.")
+    response = model.generate_content(prompt)
+    if response.candidates and len(response.candidates) > 0:
+        if response.candidates[0].content.parts and len(response.candidates[0].content.parts) > 0:
+            return response.candidates[0].content.parts[0].text
         else:
-            print("Nenhum candidato válido encontrado.")
-    except Exception as e:
-        print(f"Erro ao usar o modelo Gemini: {e}")
-    
-    # Se a tradução falhar, tentar com a API do ChatGPT
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=500
-        )
-        return response.choices[0].text.strip()
-    except Exception as e:
-        print(f"Erro ao usar o ChatGPT: {e}")
-    
+            print("Nenhuma parte de conteúdo encontrada na resposta.")
+    else:
+        print("Nenhum candidato válido encontrado.")
     return None
 
-# Função para baixar a última postagem do Instagram da NASA e traduzi-la
+# Função para baixar e traduzir a última postagem do Instagram da NASA
 def baixar_e_traduzir_post(cl, username, legendas_postadas):
     yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
     medias = cl.user_medias(cl.user_id_from_username(username), 10)
@@ -111,46 +91,22 @@ def baixar_e_traduzir_post(cl, username, legendas_postadas):
                 print(f"Legenda já postada anteriormente: {caption}")
                 continue
             prompt_nasa = f"Given the following scientific text from a reputable source (NASA) in English, translate it accurately and fluently into grammatically correct Brazilian Portuguese while preserving the scientific meaning: {caption}"
-            traducao_nasa = gerar_traducao(prompt_nasa) or caption
-
-            image_path = f"imagem_{username}.jpg"
-            response = requests.get(image_url)
-            if response.status_code == 200:
-                with open(image_path, 'wb') as f:
-                    f.write(response.content)
-                return image_path, traducao_nasa, caption
+            traducao_nasa = gerar_traducao(prompt_nasa)
+            if traducao_nasa:
+                image_path = f"imagem_{username}.jpg"
+                response = requests.get(image_url)
+                if response.status_code == 200:
+                    with open(image_path, 'wb') as f:
+                        f.write(response.content)
+                    return image_path, traducao_nasa, caption
+                else:
+                    print(f"Erro ao baixar a imagem de {username}.")
+                    return None, None, None
             else:
-                print(f"Erro ao baixar a imagem de {username}.")
+                print("Não foi possível traduzir a legenda.")
                 return None, None, None
     print(f"Nenhuma mídia válida encontrada para {username}.")
     return None, None, None
-
-# Função para baixar o vídeo e retornar o nome do arquivo baixado
-def download_video(link):
-    try:
-        youtube_object = YouTube(link)
-        video_stream = youtube_object.streams.get_highest_resolution()
-        if video_stream:
-            video_filename = video_stream.default_filename
-            video_stream.download()
-            return video_filename  # Retorna o nome do arquivo do vídeo baixado
-        else:
-            print("Nenhuma stream encontrada para o vídeo.")
-            return None
-    except Exception as e:
-        print(f"Erro ao baixar o vídeo: {e}")
-        return None  # Retorna None se o download falhar
-
-# Função para cortar o vídeo
-def cortar_video(video_path, start_time, end_time, output_path):
-    try:
-        with VideoFileClip(video_path) as video:
-            video_cortado = video.subclip(start_time, end_time)
-            video_cortado.write_videofile(output_path, codec="libx264")
-        return output_path
-    except Exception as e:
-        print(f"Erro ao cortar o vídeo: {e}")
-        return None
 
 # Get the picture, explanation, and/or video thumbnail
 URL_APOD = "https://api.nasa.gov/planetary/apod"
@@ -161,7 +117,6 @@ params = {
 }
 response = requests.get(URL_APOD, params=params).json()
 site = response.get('url')
-thumbs = response.get('thumbnail_url')
 media_type = response.get('media_type')
 explanation = response.get('explanation')
 title = response.get('title')
@@ -183,7 +138,6 @@ try:
 Fonte: {site}
 
 #NASA #APOD #Astronomia #Espaço #Astrofotografia"""
-
     else:
         raise AttributeError("A tradução combinada não foi gerada.")
 except AttributeError as e:
@@ -215,18 +169,11 @@ if media_type == 'image':
 elif media_type == 'video':
     # Retrieve the video
     video_file = download_video(site)
-    
-    # Retrieve the thumbs
-    urllib.request.urlretrieve(thumbs, 'apodtoday.jpeg')
-    image = "apodtoday.jpeg"
 
     if video_file:
-        video_file_cortado = cortar_video(video_file, 0, 60, "video_cortado.mp4")
-        if video_file_cortado:
-            video_file = video_file_cortado
-
         # Post the video on Instagram
         try:
+            instagram_client = logar_instagram()
             post_instagram_photo(instagram_client, video_file, insta_string)
         except Exception as e:
             print(f"Erro ao postar vídeo no Instagram: {e}")
@@ -238,6 +185,14 @@ else:
 
 # Carregar legendas já postadas
 legendas_postadas = carregar_legendas_postadas()
+
+# Verificar se a tradução da APOD foi bem-sucedida antes de postar
+if traducao_combinada:
+    try:
+        post_instagram_photo(instagram_client, image, insta_string)
+    except Exception as e:
+        print(f"Erro ao postar a imagem da APOD no Instagram: {e}")
+        bot.send_message(tele_user, 'apodinsta com problema pra postar imagem da APOD')
 
 # Baixar e postar a última imagem de cada página da NASA no Instagram
 for page in nasa_pages:
